3c1ef068775f9239f591ce86d67e18d8ecc2c09e
Rakeshssrh <unknownrh2@gmail.com>
2025-05-16 16:57:58 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import { initializeSocketIO } from './lib/realtime/socket.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
