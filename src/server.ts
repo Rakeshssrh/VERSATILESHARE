6a3ebc70ce20df387581203bcc76418eeb664c01
Rakeshssrh <unknownrh2@gmail.com>
2025-05-18 19:30:21 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://versatileshare.onrender.com'],
+  origin: ['http://localhost:5173', 'https://versatileshare.onrender.com','https://versatileshare.up.railway.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
